8b4b0b92c1a285f2ef6c3348ccaab6a77f00b585
Hasan Çelik <hsn.2903@outlook.com>
2022-10-05 10:45:34 +0300

    new updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Footer, Navbar } from "./components";
+import { Footer, Navbar, Sidebar } from "./components";
 import {
   Home,
   About,
@@ -13,19 +14,22 @@ import {
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="products" element={<Products />} />
-        <Route path="products/:id" element={<SingleProduct />} />
-        <Route path="checkout" element={<Checkout />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="products" element={<Products />} />
+          <Route path="products/:id" element={<SingleProduct />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
