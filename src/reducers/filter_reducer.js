7b5e1e808e1ab300340a4f4085469fa4e9264fd8
Hasan Çelik <hsn.2903@outlook.com>
2022-10-08 09:10:26 +0300

    add sort functionality

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -7,11 +7,52 @@ import {
   UPDATE_FILTERS,
   FILTER_PRODUCTS,
   CLEAR_FILTERS,
-} from '../actions'
+} from "../actions";
 
 const filter_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  if (action.type === LOAD_PRODUCTS) {
+    return {
+      ...state,
+      allProducts: [...action.payload],
+      filteredProducts: [...action.payload],
+    };
+  }
 
-export default filter_reducer
+  if (action.type === SET_GRIDVIEW) {
+    return { ...state, gridView: true };
+  }
+  if (action.type === SET_LISTVIEW) {
+    return { ...state, gridView: false };
+  }
+
+  if (action.type === UPDATE_SORT) {
+    return { ...state, sort: action.payload };
+  }
+
+  if (action.type === SORT_PRODUCTS) {
+    const { sort, filteredProducts } = state;
+    let tempProducts = [...filteredProducts];
+    if (sort === "price-lowest") {
+      tempProducts = tempProducts.sort((a, b) => a.price - b.price);
+    }
+    if (sort === "price-highest") {
+      tempProducts = tempProducts.sort((a, b) => b.price - a.price);
+    }
+    if (sort === "name-a") {
+      tempProducts.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
+    }
+    if (sort === "name-z") {
+      tempProducts.sort((a, b) => {
+        return b.name.localeCompare(a.name);
+      });
+    }
+
+    return { ...state, filteredProducts: tempProducts };
+  }
+
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default filter_reducer;
